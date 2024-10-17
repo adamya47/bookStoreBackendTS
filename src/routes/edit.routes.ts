ac4f592a1d1d5493aaaafe459f5a54764f387898
adamya47 <adamya47@gmail.com>
2024-10-17 18:24:55 +0530

    "bug fix:"

--- src/routes/edit.routes.ts
+++ src/routes/edit.routes.ts
@@ -3,5 +3,5 @@ import { editBookCategory,editBookName } from "../controllers/edit.controller";
 
 export const editRouter=Router();
 
-editRouter.route("/bookname").patch(editBookName)
+editRouter.route("/bookname").post(editBookName)
 editRouter.route("/category").patch(editBookCategory)
